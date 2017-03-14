fd64d31690d5ac273aca5c3748c59c199b6aac5e
andykamp <akampenes@gmail.com>
2017-03-14 10:59:18 +0100

    Lagt til popupvindu og endret skriving til databasen slik at man kan legge til fag i favoritter:)

--- app/src/main/java/com/example/queueme/ChooseSubjectStud.java
+++ app/src/main/java/com/example/queueme/ChooseSubjectStud.java
@@ -1,13 +1,21 @@
 package com.example.queueme;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -16,26 +24,40 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class ChooseSubjectStud extends AppCompatActivity {
 
-
+public class ChooseSubjectStud extends Activity {
+    // Search EditText
+    //private EditText inputSearch;
+    ArrayAdapter feedAdapter;
+    private Button popup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_subject_stud);
+        setContentView(R.layout.activity_coose_subject_ass);
 
-
+        //finer listview og setter som variabel
         final ListView l=(ListView) findViewById(R.id.listview);
+        //finner buttons
+        popup = (Button) findViewById(R.id.popup);
 
+
+        //lager listen alle fagene skal legger i
         final ArrayList<Subject> subjects = new ArrayList<Subject>();
 
+        //inputSearch = (EditText) findViewById(R.id.inputSearch);
 
 
-
-        //Henter ut fag fra databasen og legger dem i subject listen
+        //henter ut alle subjects som ligger i databasen og legger i liste
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference();
-        myRef.child("Subject").addValueEventListener(new ValueEventListener() {
+        // brukes til dialogen:
+        final DatabaseReference myRefdialog = database.getReference();
+        final DatabaseReference myRefdialog2 = database.getReference();
+        //
+        //henter info om bruker
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        myRef.child("Person").child(user.getUid()).child("FavoriteStudSubject").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 //get all of the children of this level.
@@ -50,15 +72,20 @@ public class ChooseSubjectStud extends AppCompatActivity {
 
                 }
 
-                //lager et adapter
-                ArrayAdapter feedAdapter = new ArrayAdapter(ChooseSubjectStud.this, android.R.layout.simple_list_item_1,subjects);
+                //lager arrayadapter som viser listene
+
+                feedAdapter = new ArrayAdapter(ChooseSubjectStud.this, android.R.layout.simple_list_item_1,subjects);
                 l.setAdapter(feedAdapter);
-                //definerer hva som skal skje når man trykker på en person
+                //definerer hva som skjer når man trykker på searchknappen
+
+                //lager funkjsonen når man trykker på en av knappene i listviewen
                 l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                         Subject subject = (Subject) subjects.get(position);
-                        Intent moveToDetailIntent = new Intent(ChooseSubjectStud.this, ChoosePerson.class);
+                        Intent moveToDetailIntent = new Intent(ChooseSubjectStud.this, StartSession.class);
                         // moveToDetailIntent.putExtra("bkjb", );
                         String emnekode= subject.getEmnekode();
                         String emnenavn = subject.getEmnenavn();
@@ -70,10 +97,6 @@ public class ChooseSubjectStud extends AppCompatActivity {
                         moveToDetailIntent.putExtra("emnenavn",emnenavn);
 
 
-                        //startActivityForResult(moveToDetailIntent,position);
-                        //Person Anders = new Person();
-                        //Anders.setName("nonneanders");
-                        //person.getPersons().add(Anders);
                         startActivity(moveToDetailIntent);
 
 
@@ -95,7 +118,106 @@ public class ChooseSubjectStud extends AppCompatActivity {
             }
         });
 
+        popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                final AlertDialog.Builder mbuilder = new AlertDialog.Builder(ChooseSubjectStud.this);
+                View mView= getLayoutInflater().inflate(R.layout.dialog_subject,null);
+                mbuilder.setView(mView);
+                final AlertDialog dialog = mbuilder.create();
+                dialog.show();
+                final EditText inputSearch = (EditText) mView.findViewById(R.id.inputSearch);
+                Button finish = (Button) mView.findViewById(R.id.finish);
+                final ListView listView=(ListView) mView.findViewById(R.id.listview);
+
+                //lager listen alle fagene skal legger i
+                final ArrayList<Subject> subjects = new ArrayList<Subject>();
+
+                //
+                myRef.child("Subject").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //get all of the children of this level.
+                        Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                        //shake hands with each of them
+                        for (DataSnapshot child: children){
+                            Subject subject = child.getValue(Subject.class);
+                            subjects.add(subject);
+
+
+
+                        }
+
+                        //lager arrayadapter som viser listene
+
+                        feedAdapter = new ArrayAdapter(ChooseSubjectStud.this, android.R.layout.simple_list_item_1,subjects);
+                        listView.setAdapter(feedAdapter);
+                        //definerer hva som skjer når man trykker på searchknappen
+                        inputSearch.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                            }
+
+                            @Override
+                            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                ChooseSubjectStud.this.feedAdapter.getFilter().filter(s);
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable s) {
+
+                            }
+                        });
+                        //lager funkjsonen når man trykker på en av knappene i listviewen
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Subject subject = (Subject) subjects.get(position);
+                                String emnekode= subject.getEmnekode();
+                                String emnenavn = subject.getEmnenavn();
+                                //henter brukerdata
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                                myRefdialog2.child("Person").child(user.getUid()).child("FavoriteStudSubject").push().setValue(subject);
+                                Toast.makeText(ChooseSubjectStud.this,"Subject added to favorites",Toast.LENGTH_SHORT).show();
+
+
+
+
+
+                            }
+                        });
+
+
+
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                //
+                finish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+            }
+
+        });
 
     }
+
+
 }
--- app/src/main/java/com/example/queueme/CooseSubjectAss.java
+++ app/src/main/java/com/example/queueme/CooseSubjectAss.java
@@ -1,6 +1,7 @@
 package com.example.queueme;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,9 +9,13 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,8 +27,9 @@ import java.util.ArrayList;
 
 public class CooseSubjectAss extends Activity {
     // Search EditText
-    private EditText inputSearch;
+    //private EditText inputSearch;
     ArrayAdapter feedAdapter;
+    private Button popup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,16 +37,27 @@ public class CooseSubjectAss extends Activity {
 
         //finer listview og setter som variabel
         final ListView l=(ListView) findViewById(R.id.listview);
+        //finner buttons
+        popup = (Button) findViewById(R.id.popup);
+
+
         //lager listen alle fagene skal legger i
         final ArrayList<Subject> subjects = new ArrayList<Subject>();
 
-        inputSearch = (EditText) findViewById(R.id.inputSearch);
+        //inputSearch = (EditText) findViewById(R.id.inputSearch);
 
 
         //henter ut alle subjects som ligger i databasen og legger i liste
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference();
-        myRef.child("Subject").addValueEventListener(new ValueEventListener() {
+        // brukes til dialogen:
+        final DatabaseReference myRefdialog = database.getReference();
+        final DatabaseReference myRefdialog2 = database.getReference();
+        //
+        //henter info om bruker
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        myRef.child("Person").child(user.getUid()).child("FavoriteAssSubject").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 //get all of the children of this level.
@@ -60,22 +77,7 @@ public class CooseSubjectAss extends Activity {
                  feedAdapter = new ArrayAdapter(CooseSubjectAss.this, android.R.layout.simple_list_item_1,subjects);
                 l.setAdapter(feedAdapter);
                 //definerer hva som skjer når man trykker på searchknappen
-                inputSearch.addTextChangedListener(new TextWatcher() {
-                    @Override
-                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-                    }
-
-                    @Override
-                    public void onTextChanged(CharSequence s, int start, int before, int count) {
-                        CooseSubjectAss.this.feedAdapter.getFilter().filter(s);
-                    }
-
-                    @Override
-                    public void afterTextChanged(Editable s) {
-
-                    }
-                });
                 //lager funkjsonen når man trykker på en av knappene i listviewen
                 l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -116,7 +118,106 @@ public class CooseSubjectAss extends Activity {
             }
         });
 
+        popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                final AlertDialog.Builder mbuilder = new AlertDialog.Builder(CooseSubjectAss.this);
+                View mView= getLayoutInflater().inflate(R.layout.dialog_subject,null);
+                mbuilder.setView(mView);
+                final AlertDialog dialog = mbuilder.create();
+                dialog.show();
+                final EditText inputSearch = (EditText) mView.findViewById(R.id.inputSearch);
+                Button finish = (Button) mView.findViewById(R.id.finish);
+                final ListView listView=(ListView) mView.findViewById(R.id.listview);
+
+                //lager listen alle fagene skal legger i
+                final ArrayList<Subject> subjects = new ArrayList<Subject>();
+
+                //
+                myRef.child("Subject").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        //get all of the children of this level.
+                        Iterable<DataSnapshot> children = dataSnapshot.getChildren();
+
+                        //shake hands with each of them
+                        for (DataSnapshot child: children){
+                            Subject subject = child.getValue(Subject.class);
+                            subjects.add(subject);
+
+
+
+                        }
+
+                        //lager arrayadapter som viser listene
+
+                        feedAdapter = new ArrayAdapter(CooseSubjectAss.this, android.R.layout.simple_list_item_1,subjects);
+                        listView.setAdapter(feedAdapter);
+                        //definerer hva som skjer når man trykker på searchknappen
+                        inputSearch.addTextChangedListener(new TextWatcher() {
+                            @Override
+                            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                            }
+
+                            @Override
+                            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                                CooseSubjectAss.this.feedAdapter.getFilter().filter(s);
+                            }
+
+                            @Override
+                            public void afterTextChanged(Editable s) {
+
+                            }
+                        });
+                        //lager funkjsonen når man trykker på en av knappene i listviewen
+                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                Subject subject = (Subject) subjects.get(position);
+                                String emnekode= subject.getEmnekode();
+                                String emnenavn = subject.getEmnenavn();
+                                //henter brukerdata
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                                myRefdialog2.child("Person").child(user.getUid()).child("FavoriteAssSubject").push().setValue(subject);
+                                Toast.makeText(CooseSubjectAss.this,"Subject added to favorites",Toast.LENGTH_SHORT).show();
+
+
+
+
+
+                            }
+                        });
+
+
+
+
+
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+                //
+                finish.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+            }
+
+        });
 
     }
+
+
 }
