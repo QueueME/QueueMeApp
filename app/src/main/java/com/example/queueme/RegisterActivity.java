746307c7989dbafbaab8bdc5d60b9c52c756473f
andykamp <akampenes@gmail.com>
2017-03-19 22:15:14 +0100

    Lagt til ny login og gjort sånn at en person legges til databasen

--- app/src/main/java/com/example/queueme/RegisterActivity.java
+++ app/src/main/java/com/example/queueme/RegisterActivity.java
@@ -1,15 +1,24 @@
 package com.example.queueme;
 
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private EditText fullname;
+    private EditText name;
     private CheckBox man, woman;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +27,33 @@ public class RegisterActivity extends AppCompatActivity {
 
         man = (CheckBox) findViewById(R.id.checkBox);
         woman = (CheckBox) findViewById(R.id.checkBox2);
-        fullname = (EditText) findViewById(R.id.fullname);
+        name = (EditText) findViewById(R.id.fullname);
+        button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createPersonFromUser();
+                startActivity(new Intent(RegisterActivity.this, StudOrAss.class));
+                finish();
+            }
+        });
+
+    }
+    //lager en person i databasen med fulltnavn slik at vi kan bruke fullt navn senere
+    private void createPersonFromUser(){
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        Person person =new Person();
+        person.setName(name.getText().toString());
+        person.setEmail(user.getEmail());
+        person.setUid(user.getUid());
+        person.setTime_to_stop("0");
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef= database.getReference("Person");
+        myRef.child(user.getUid()).setValue(person);
 
     }
 }
